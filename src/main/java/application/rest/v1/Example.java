ca55b3ea69e6af4b3ee83792cc9d10c1417c17d8
CJ <cjohannessen@us.ibm.com>
2017-04-06 14:14:01 -0700

    Better greeting

--- src/main/java/application/rest/v1/Example.java
+++ src/main/java/application/rest/v1/Example.java
@@ -16,7 +16,7 @@ public class Example {
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   public Response hello() {
-    return Response.ok("Hello World").build();
+    return Response.ok("Hello Kevin").build();
 
   }
 
